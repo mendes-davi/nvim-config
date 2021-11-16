892487b7ac4d44a5b75c87e31db85bad43de3271
mendes-davi <dmendes@aluno.unb.br>
2021-11-16 20:58:04 -0300

    feat: matlab tree-sitter parser

--- lua/config/nvim-treesitter.lua
+++ lua/config/nvim-treesitter.lua
@@ -38,4 +38,14 @@ hlmap["punctuation.delimiter"] = "Delimiter"
 hlmap["punctuation.bracket"] = nil
 
 -- nomad
-require("nvim-treesitter.parsers").get_parser_configs().hcl.used_by = { "nomad", "terraform", "tf" }
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.hcl.used_by = { "nomad", "terraform", "tf" }
+
+parser_config.matlab = {
+  install_info = {
+    url = "https://github.com/mstanciu552/tree-sitter-matlab.git",
+    files = { "src/parser.c" },
+    branch= 'main'
+  },
+  filetype = "matlab", -- if filetype does not agrees with parser name
+}
