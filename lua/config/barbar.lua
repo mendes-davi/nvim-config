0ed663e75e9aad1b425bdf98ea39125acdf1084a
mendes-davi <dmendes@aluno.unb.br>
2022-05-16 21:51:54 -0300

    fix: barbar mappings

--- lua/config/barbar.lua
+++ lua/config/barbar.lua
@@ -1,38 +1,38 @@
 local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
+local opts = { noremap = true }
 
 -- Move to previous/next
-map("n", "<A-[>", ":BufferPrevious<CR>", opts)
-map("n", "<A-]>", ":BufferNext<CR>", opts)
+map("n", "<A-[>", "<cmd>BufferPrevious<CR>", opts)
+map("n", "<A-]>", "<cmd>BufferNext<CR>", opts)
 -- Re-order to previous/next
-map("n", "<A-<>", ":BufferMovePrevious<CR>", opts)
-map("n", "<A->>", " :BufferMoveNext<CR>", opts)
+map("n", "<A-<>", "<cmd>BufferMovePrevious<CR>", opts)
+map("n", "<A->>", "<cmd>BufferMoveNext<CR>", opts)
 -- Goto buffer in position...
-map("n", "<A-1>", ":BufferGoto 1<CR>", opts)
-map("n", "<A-2>", ":BufferGoto 2<CR>", opts)
-map("n", "<A-3>", ":BufferGoto 3<CR>", opts)
-map("n", "<A-4>", ":BufferGoto 4<CR>", opts)
-map("n", "<A-5>", ":BufferGoto 5<CR>", opts)
-map("n", "<A-6>", ":BufferGoto 6<CR>", opts)
-map("n", "<A-7>", ":BufferGoto 7<CR>", opts)
-map("n", "<A-8>", ":BufferGoto 8<CR>", opts)
-map("n", "<A-9>", ":BufferGoto 9<CR>", opts)
-map("n", "<A-0>", ":BufferLast<CR>", opts)
+map("n", "<A-1>", "<cmd>BufferGoto 1<CR>", opts)
+map("n", "<A-2>", "<cmd>BufferGoto 2<CR>", opts)
+map("n", "<A-3>", "<cmd>BufferGoto 3<CR>", opts)
+map("n", "<A-4>", "<cmd>BufferGoto 4<CR>", opts)
+map("n", "<A-5>", "<cmd>BufferGoto 5<CR>", opts)
+map("n", "<A-6>", "<cmd>BufferGoto 6<CR>", opts)
+map("n", "<A-7>", "<cmd>BufferGoto 7<CR>", opts)
+map("n", "<A-8>", "<cmd>BufferGoto 8<CR>", opts)
+map("n", "<A-9>", "<cmd>BufferGoto 9<CR>", opts)
+map("n", "<A-0>", "<cmd>BufferLast<CR>", opts)
 -- Close buffer
-map("n", "<A-e>", ":BufferClose<CR>", opts)
+map("n", "<A-e>", "<cmd>BufferClose<CR>", opts)
 -- Wipeout buffer
-map("n", "<A-E>", ":BufferWipeout!<CR>", opts)
+map("n", "<A-E>", "<cmd>BufferWipeout!<CR>", opts)
 
 -- Close commands
---                 :BufferCloseAllButCurrent<CR>
---                 :BufferCloseBuffersLeft<CR>
---                 :BufferCloseBuffersRight<CR>
+--                 <cmd>BufferCloseAllButCurrent<CR>
+--                 <cmd>BufferCloseBuffersLeft<CR>
+--                 <cmd>BufferCloseBuffersRight<CR>
 -- Magic buffer-picking mode
-map("n", "<A-u>", ":BufferPick<CR>", opts)
+map("n", "<A-u>", "<cmd>BufferPick<CR>", opts)
 
 -- Sort automatically by...
--- map('n', '<Space>bd', ':BufferOrderByDirectory<CR>', opts)
--- map('n', '<Space>bl', ':BufferOrderByLanguage<CR>', opts)
+-- map('n', '<Space>bd', '<cmd>BufferOrderByDirectory<CR>', opts)
+-- map('n', '<Space>bl', '<cmd>BufferOrderByLanguage<CR>', opts)
 
 -- Other:
 -- :BarbarEnable - enables barbar (enabled by default)
