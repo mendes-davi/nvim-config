af978ee1377a86ff445a33320511b753825c1366
mendes-davi <dmendes@aluno.unb.br>
2022-05-25 22:10:56 -0300

    refactor: nvim-tree config

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -1,28 +1,18 @@
--- NvimTreeOpen and NvimTreeClose are also available if you need them
--- a list of groups can be found at `:help nvim_tree_highlight`
-vim.cmd [[highlight NvimTreeFolderIcon guibg=#1b95e0]]
-
--- jump to the main window (startify)
--- :h startify-faq-06
 Variable.g {
 	nvim_tree_side = "left", -- left by default
 	nvim_tree_width = 30, -- 30 by default
-	-- nvim_tree_auto_open = 0, -- 0 by default, opens the tree when typing `vim $DIR` or `vim`
-	-- nvim_tree_auto_close = 1, -- 0 by default, closes the tree when it's the last window
-	-- nvim_tree_follow = 1, -- 0 by default, this option allows the cursor to be updated when entering a buffer
 	nvim_tree_git_hl = 1, -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
 	nvim_tree_root_folder_modifier = ":~", -- This is the default. See :help filename-modifiers for more options
-	-- nvim_tree_tab_open = 1, -- 0 by default, will open the tree when entering a new tab and the tree was previously open
 	nvim_tree_allow_resize = 1, -- 0 by default, will not resize the tree when opening a file
+	--If 0, do not show the icons for one of 'git' 'folder' and 'files'
+	--1 by default, notice that if 'files' is 1, it will only display
+	--if nvim-web-devicons is installed and on your runtimepath
 	nvim_tree_show_icons = {
 		git = 1,
 		folders = 1,
 		files = 1,
 		folder_arrows = 1,
 	},
-	--If 0, do not show the icons for one of 'git' 'folder' and 'files'
-	--1 by default, notice that if 'files' is 1, it will only display
-	--if nvim-web-devicons is installed and on your runtimepath
 	nvim_tree_special_files = {
 		["go.mod"] = 1,
 		["Cargo.toml"] = 1,
@@ -32,26 +22,84 @@ Variable.g {
 	}, -- List of filenames that gets highlighted with NvimTreeSpecialFile
 }
 
+local lib = require "nvim-tree.lib"
+local view = require "nvim-tree.view"
+
+local function collapse_all()
+	require("nvim-tree.actions.collapse-all").fn()
+end
+
+local function edit_or_open()
+	-- open as vsplit on current node
+	local action = "edit"
+	local node = lib.get_node_at_cursor()
+
+	-- Just copy what's done normally with vsplit
+	if node.link_to and not node.nodes then
+		require("nvim-tree.actions.open-file").fn(action, node.link_to)
+		view.close() -- Close the tree if file was opened
+	elseif node.nodes ~= nil then
+		lib.expand_or_collapse(node)
+	else
+		require("nvim-tree.actions.open-file").fn(action, node.absolute_path)
+		view.close() -- Close the tree if file was opened
+	end
+end
+
+local function vsplit_preview()
+	-- open as vsplit on current node
+	local action = "vsplit"
+	local node = lib.get_node_at_cursor()
+
+	-- Just copy what's done normally with vsplit
+	if node.link_to and not node.nodes then
+		require("nvim-tree.actions.open-file").fn(action, node.link_to)
+	elseif node.nodes ~= nil then
+		lib.expand_or_collapse(node)
+	else
+		require("nvim-tree.actions.open-file").fn(action, node.absolute_path)
+	end
+end
+
 -- https://github.com/kyazdani42/nvim-tree.lua#setup
 require("nvim-tree").setup {
-	nvim_tree_quit_on_open = 1, -- 0 by default, closes the tree when you open a file
-	nvim_tree_window_picker_exclude = {
-		filetype = {
-			"packer",
-			"qf",
-		},
-		buftype = {
-			"terminal",
+	hijack_netrw = false,
+	hijack_directories = {
+		enable = false,
+		auto_open = false,
+	},
+	view = {
+		mappings = {
+			custom_only = false,
+			list = {
+				{ key = "l", action = "edit", action_cb = edit_or_open },
+				{ key = "L", action = "vsplit_preview", action_cb = vsplit_preview },
+				{ key = "h", action = "close_node" },
+				{ key = "H", action = "collapse_all", action_cb = collapse_all },
+			},
 		},
 	},
-	-- closes neovim automatically when the tree is the last **WINDOW** in the view
-	auto_close = false,
-	-- 0 by default, this option hides files and folders starting with a dot `.`
-	nvim_tree_hide_dotfiles = 1,
-	nvim_tree_ignore = { ".git", "node_modules", ".cache", ".idea" }, -- empty by default
-	renderer = {
-		ident_markers = {
-			enable = false,
+	actions = {
+		open_file = {
+			quit_on_open = true,
+			window_picker = {
+				exclude = {
+					filetype = { "packer", "qf", "notify", "diff" },
+					buftype = { "terminal", "nofile", "help" },
+				},
+			},
 		},
+	},
+	filters = {
+		custom = { ".git", "node_modules", ".cache", ".idea", "__pycache__" },
+	},
+	git = {
+		enable = false,
 	},
 }
+
+-- Auto-close
+vim.api.nvim_create_autocmd("BufEnter", {
+	command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif",
+	nested = true,
+})
