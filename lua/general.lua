3863a26e7d3faa9a190479735d8951fcecbf3ad8
mendes-davi <dmendes@aluno.unb.br>
2022-05-31 22:15:12 -0300

    feat: toggle quickfix mapping

--- lua/general.lua
+++ lua/general.lua
@@ -213,6 +213,30 @@ cnoremap {
 }
 
 nnoremap {
+	"<leader>X",
+	function()
+		local qf_open = false
+		for _, win in pairs(vim.fn.getwininfo()) do
+			if win["quickfix"] == 1 then
+				qf_open = true
+			end
+		end
+		if qf_open == true then
+			vim.cmd "cclose"
+			return
+		end
+		if not vim.tbl_isempty(vim.fn.getqflist()) then
+			vim.cmd "copen"
+			if vim.b.bqf_enabled or false then
+				vim.api.nvim_feedkeys([['"]], "im", false)
+			end
+		else
+			vim.notify("[QF] - is empty", vim.log.levels.INFO)
+		end
+	end,
+}
+
+nnoremap {
 	"<leader>hello",
 	function()
 		print "Hello world, from lua"
