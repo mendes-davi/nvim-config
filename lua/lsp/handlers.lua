71474bb77182848ece6356b25a8c56d174cca6a5
mendes-davi <dmendes@aluno.unb.br>
2022-06-08 23:32:16 -0300

    fix: lsp handler for rename

--- lua/lsp/handlers.lua
+++ lua/lsp/handlers.lua
@@ -42,56 +42,65 @@ lsp.handlers["textDocument/hover"] = lsp.with(vim.lsp.handlers.hover, {
 local function qf_rename()
 	local position_params = vim.lsp.util.make_position_params()
 	position_params.oldName = vim.fn.expand "<cword>"
-	position_params.newName = vim.fn.input("Rename To> ", position_params.oldName)
-
-	vim.lsp.buf_request(0, "textDocument/rename", position_params, function(err, result, ...)
-		if not result or not result.changes then
-			require "notify"(string.format "could not perform rename", "error", {
-				title = string.format("[lsp] rename: %s -> %s", position_params.oldName, position_params.newName),
-				timeout = 500,
-			})
-
+	vim.ui.input({ prompt = "Rename To> ", default = position_params.oldName }, function(input)
+		if not input or #input == 0 then
 			return
 		end
 
-		vim.lsp.handlers["textDocument/rename"](err, result, ...)
-
-		local notification, entries = "", {}
-		local num_files, num_updates = 0, 0
-		for uri, edits in pairs(result.changes) do
-			num_files = num_files + 1
-			local bufnr = vim.uri_to_bufnr(uri)
-
-			for _, edit in ipairs(edits) do
-				local start_line = edit.range.start.line + 1
-				local line = vim.api.nvim_buf_get_lines(bufnr, start_line - 1, start_line, false)[1]
-
-				num_updates = num_updates + 1
-				table.insert(entries, {
-					bufnr = bufnr,
-					lnum = start_line,
-					col = edit.range.start.character + 1,
-					text = line,
+		position_params.newName = input
+		vim.lsp.buf_request(0, "textDocument/rename", position_params, function(err, result, ...)
+			if not result then
+				require "notify"(string.format "could not perform rename", "error", {
+					title = string.format("[lsp] rename: %s -> %s", position_params.oldName, position_params.newName),
+					timeout = 500,
 				})
+
+				return
 			end
 
-			local short_uri = string.sub(vim.uri_to_fname(uri), #vim.fn.getcwd() + 2)
-			notification = notification .. string.format("made %d change(s) in %s", #edits, short_uri)
-		end
+			vim.lsp.handlers["textDocument/rename"](err, result, ...)
+            if not result.changes then
+                return
+            end
 
-		-- print(string.format("updated %d instance(s) in %d file(s)", num_updates, num_files))
-		require "notify"(notification, "info", {
-			title = string.format("[lsp] rename: %s -> %s", position_params.oldName, position_params.newName),
-			timeout = 2500,
-		})
+			local notification, entries = "", {}
+			local num_files, num_updates = 0, 0
+            -- FIXME: pyright sends a different `result` table arrangement
+			for uri, edits in pairs(result.changes or {}) do
+				num_files = num_files + 1
+				local bufnr = vim.uri_to_bufnr(uri)
 
-		if num_files > 1 then
-			vim.fn.setqflist(entries, "r")
-			vim.cmd [[
+				for _, edit in ipairs(edits) do
+					local start_line = edit.range.start.line + 1
+					local line = vim.api.nvim_buf_get_lines(bufnr, start_line - 1, start_line, false)[1]
+
+					num_updates = num_updates + 1
+					table.insert(entries, {
+						bufnr = bufnr,
+						lnum = start_line,
+						col = edit.range.start.character + 1,
+						text = line,
+					})
+				end
+
+				local short_uri = string.sub(vim.uri_to_fname(uri), #vim.fn.getcwd() + 2)
+				notification = notification .. string.format("made %d change(s) in %s", #edits, short_uri)
+			end
+
+			-- print(string.format("updated %d instance(s) in %d file(s)", num_updates, num_files))
+			require "notify"(notification, "info", {
+				title = string.format("[lsp] rename: %s -> %s", position_params.oldName, position_params.newName),
+				timeout = 2500,
+			})
+
+			if num_files > 1 then
+				vim.fn.setqflist(entries, "r")
+				vim.cmd [[
                         belowright copen
                         wincmd p
                     ]]
-		end
+			end
+		end)
 	end)
 end
 lsp.buf.rename = qf_rename
