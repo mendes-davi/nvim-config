cdfafe5c9155459b8ab670c61655eca3dadf57e2
mendes-davi <dmendes@aluno.unb.br>
2022-06-15 16:17:07 -0300

    feat: treesitter latex parser & minor cleanup

--- lua/config/nvim-treesitter.lua
+++ lua/config/nvim-treesitter.lua
@@ -1,8 +1,8 @@
 require("nvim-treesitter.configs").setup {
-	ensure_installed = { "query", "c", "go", "rust", "php", "python", "lua", "json", "toml", "vue", "css", "html", "bash", "hcl" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = { "query", "c", "go", "rust", "php", "python", "lua", "json", "toml", "vue", "css", "html", "bash", "hcl", "latex" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
 	highlight = {
 		enable = true, -- false will disable the whole extension
-		disable = { "markdown" }, -- list of language that will be disabled
+		disable = { "markdown", "latex" }, -- list of language that will be disabled
 	},
 	playground = {
 		enable = true,
@@ -81,27 +81,11 @@ require("nvim-treesitter.configs").setup {
 	},
 }
 
--- fixup nvim-treesitter cause luochen1990/rainbow not working problem
--- see https://github.com/nvim-treesitter/nvim-treesitter/issues/123#issuecomment-651162962
--- https://github.com/nvim-treesitter/nvim-treesitter/issues/654#issuecomment-727562988
--- https://github.com/luochen1990/rainbow/issues/151#issuecomment-677644891
--- require "nvim-treesitter.highlight"
--- vim.TSHighlighter is removed, please use vim.treesitter.highlighter
--- see https://github.com/neovim/neovim/pull/14145/commits/b5401418768af496ef23b790f700a44b61ad784d
--- local hlmap = vim.treesitter.highlighter.hl_map
--- deactivate highlight of TSPunctBracket
--- highlight link TSPunctBracket Normal
--- hlmap.error = nil
--- hlmap["punctuation.delimiter"] = "Delimiter"
--- hlmap["punctuation.bracket"] = nil
-
--- nomad
+-- MATLAB
 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-parser_config.hcl.filetyp_to_parsename = { "nomad", "terraform", "tf" }
-
 parser_config.matlab = {
 	install_info = {
-		url = "https://github.com/mstanciu552/tree-sitter-matlab.git",
+		url = "https://github.com/mendes-davi/tree-sitter-matlab.git",
 		files = { "src/parser.c" },
 		branch = "main",
 	},
