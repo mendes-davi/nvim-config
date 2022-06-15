ea48385430a0ac8e6fe2aa1e6a9bbc0de405a87c
mendes-davi <dmendes@aluno.unb.br>
2022-06-15 16:05:32 -0300

    fix: unnamed register miniyank-autoput override

--- lua/config/nvim-miniyank.lua
+++ lua/config/nvim-miniyank.lua
@@ -1,5 +1,14 @@
 map { "p", "<Plug>(miniyank-autoput)" }
 map { "P", "<Plug>(miniyank-autoPut)" }
+-- Don't touch unnamed register when pasting over visual selection
+xnoremap {
+	"p",
+	function()
+		return '<Plug>(miniyank-autoput)gv"' .. vim.v.register .. "y"
+	end,
+	expr = true,
+}
+
 map { "<leader>n", "<Plug>(miniyank-cycle)" }
 map { "<leader>N", "<Plug>(miniyank-cycleback)" }
 -- " the standard "+gP and "+y commands very difficult to use
