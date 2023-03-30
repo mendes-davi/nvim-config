0821349a95586f02dbe6451c3ca7a1d04ac91010
mendes-davi <davi.mendes@aluno.unb.br>
2023-03-30 00:13:09 -0300

    refactor: barbar migrate to bufferline.setup

--- lua/config/barbar.lua
+++ lua/config/barbar.lua
@@ -39,7 +39,7 @@ map("n", "<A-u>", "<cmd>BufferPick<CR>", opts)
 -- :BarbarDisable - very bad command, should never be used
 
 -- Set barbar's options
-vim.g.bufferline = {
+require("bufferline").setup {
 	-- Enable/disable animations
 	animation = true,
 
@@ -70,26 +70,45 @@ vim.g.bufferline = {
 	exclude_name = { "package.json" },
 
 	-- Enable/disable icons
-	-- if set to 'numbers', will show buffer index in the tabline
-	-- if set to 'both', will show buffer index and icons in the tabline
-	icons = "both",
+	icons = {
+		-- Configure the base icons on the bufferline.
+		buffer_index = true,
+		buffer_number = false,
+		button = "",
+		-- Enables / disables diagnostic symbols
+		diagnostics = {
+			[vim.diagnostic.severity.ERROR] = { enabled = false },
+			[vim.diagnostic.severity.WARN] = { enabled = false },
+			[vim.diagnostic.severity.INFO] = { enabled = false },
+			[vim.diagnostic.severity.HINT] = { enabled = false },
+		},
+		filetype = {
+			-- Sets the icon's highlight group.
+			-- If false, will use nvim-web-devicons colors
+			custom_colors = false,
 
-	-- If set, the icon color will follow its corresponding buffer
-	-- highlight group. By default, the Buffer*Icon group is linked to the
-	-- Buffer* group (see Highlighting below). Otherwise, it will take its
-	-- default value as defined by devicons.
-	icon_custom_colors = false,
+			-- Requires `nvim-web-devicons` if `true`
+			enabled = true,
+		},
+		separator = { left = "▎", right = "" },
 
-	-- Configure icons on the bufferline.
-	icon_separator_active = "▎",
-	icon_separator_inactive = "▎",
-	icon_close_tab = "",
-	icon_close_tab_modified = "●",
-	icon_pinned = "車",
+		-- Configure the icons on the bufferline when modified or pinned.
+		-- Supports all the base icon options.
+		modified = { button = "●" },
+		pinned = { button = "車" },
+
+		-- Configure the icons on the bufferline based on the visibility of a buffer.
+		-- Supports all the base icon options, plus `modified` and `pinned`.
+		alternate = { filetype = { enabled = false } },
+		current = { buffer_index = true },
+		inactive = { button = "×" },
+		visible = { modified = { buffer_number = false } },
+	},
 
 	-- If true, new buffers will be inserted at the end of the list.
 	-- Default is to insert after current buffer.
 	insert_at_end = false,
+	insert_at_start = false,
 
 	-- Sets the maximum padding width with which to surround each tab
 	maximum_padding = 100,
