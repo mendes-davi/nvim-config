2a23b7a87675f38a0af50c39a0dd19e5f8b4d3a5
mendes-davi <davi.mendes@aluno.unb.br>
2024-08-25 00:29:08 -0300

    feat: include marks in statuscol

--- lua/config/statuscol.lua
+++ lua/config/statuscol.lua
@@ -3,6 +3,18 @@ local builtin = require "statuscol.builtin"
 require("statuscol").setup {
 	segments = {
 		{ text = { "%s" }, click = "v:lua.ScSa" },
+		{
+			text = {
+				function(args)
+					for _, mark in ipairs(vim.fn.getmarklist(args.buf)) do
+						if mark.pos[2] == args.lnum then
+							return mark.mark:sub(-1)
+						end
+					end
+					return ""
+				end,
+			},
+		},
 		{ text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
 		{
 			text = { " ", builtin.foldfunc, " " },
